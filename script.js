c83dc6ba83ba51675746bd4635b101f066ef6f57
Prabh231 <200651351@student.georgianc.on.ca>
2026-04-09 17:58:14 -0400

    Update script.js
    used this variable to detect when form is submitted

---
+++ script.js
@@ -0,0 +1,4 @@
+// This section selects all the elements from the HTML so they can be used in JavaScript.
+
+// This variable stores the contact form element.
+let contactForm = document.querySelector(".contact-form");
